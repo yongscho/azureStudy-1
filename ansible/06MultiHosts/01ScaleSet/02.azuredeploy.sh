3e520a8fc8ae60dc529324516a1f3925b11fd8fb
Minsoo Jo <minsoojo@Minsooui-MacBook-Pro.local>
2017-12-05 17:58:48 +0900

    add pubic dnsname

--- ansible/06MultiHosts/01ScaleSet/02.azuredeploy.sh
+++ ansible/06MultiHosts/01ScaleSet/02.azuredeploy.sh
@@ -2,6 +2,39 @@
 set -euox pipefail
 IFS=$'\n\t'
 
+usage() { echo "Usage: $0 -g <resourceGroupName>" 1>&2; exit 1; }
+
+declare subscriptionId="7b13dc94-2b54-4cdf-a247-bbdebdb97f4f"
+declare resourceGroupName=""
+declare resourceGroupLocation=koreacentral
+
+# Initialize parameters specified from command line
+while getopts ":g:" arg; do
+    case "${arg}" in
+        g)
+            resourceGroupName=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$resourceGroupName" ]]; then
+    echo "ResourceGroupName:"
+    read resourceGroupName
+    [[ "${resourceGroupName:?}" ]]
+fi
+
+if [ $(az group exists --name $resourceGroupName) == 'false' ]; then
+    echo "Resource group with name" $resourceGroupName "could not be found. Creating new resource group.."
+    (
+        set -x
+        az group create --name $resourceGroupName --location $resourceGroupLocation 1> /dev/null
+    )
+    else
+    echo "Using existing resource group..."
+fi
+
 az group deployment create \
+--resource-group $resourceGroupName \
 --template-file 02.azuredeploy.json \
 --parameters @02.azuredeploy.parameters.json
